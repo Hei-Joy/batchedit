6d3710d00f0ef23ad5713c94f43bb2de97567483
Hei-Joy <1091251415@qq.com>
2020-06-08 19:48:16 +0800

    http

--- src/main/java/com/moguding/batchedit/util/HttpUtil.java
+++ src/main/java/com/moguding/batchedit/util/HttpUtil.java
@@ -1,24 +1,16 @@
 package com.moguding.batchedit.util;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
